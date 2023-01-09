8d4710b966cbf16cd756322f509690d3086f1ece
evamgarcia <emgarcia2911@gmail.com>
2023-01-09 17:02:19 +0100

    Update artefactososvaliosos.py

--- artefactososvaliosos.py
+++ artefactososvaliosos.py
@@ -15,6 +15,16 @@ class artefactosvaliosos:
 
 
 collar = artefactosvaliosos(3, "collar", 105, "13/11/2021")
-print(collar)
 pendientes = artefactosvaliosos(5, "pendientes", 50, "29/11/2023")
-print(pendientes)
+reloj = artefactosvaliosos(4, "reloj", 256, "12/12/2020")
+
+objetos = [collar, pendientes, reloj]
+objetos.sort(key=lambda x: x.fecha_caducidad)
+for objeto in objetos:
+    print(objeto)
+    
+
+
+#Prueba a mostrar los datos de algunos artefactos valiosos ordenados por su fecha de caducidad 
+#y a modificar algún valor, por ejemplo, prueba a modificar el precio de un de la conserva
+
